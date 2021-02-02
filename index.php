2c8373ec54df48485614f788ca4252f2c112e541
Scrirock <73163680+Scrirock@users.noreply.github.com>
2021-02-02 16:26:50 +0100

    frfr

--- index.php
+++ index.php
@@ -4,13 +4,10 @@
  * 2. Ajoutez au moins une adresse mail ( la vôtre ? ) au tableau $to.
  * 3. Envoyez ce mail au format HTML à toutes les personnes de $ to qui ont souscrit à votre newsletter.
  * Bonus, pour chaque adresse mail indiquer si le mail a bien été envoyer, ou faire un tableau contenant les succès / erreurs et l'afficher après envoi
- *    ex: ['j.conan@fondationface.org' => true, 'toto@fondationface.org' => false] => à vous de trouver la suite !
+ *
  */
 
-$to = [
-    'j.conan@fondationface.org',
-    ...
-];
+$to = '';
 
 $html = '
     <html lang="fr">
@@ -19,9 +16,13 @@ $html = '
         </head>
         <body>
             <div>
-                <!-- Vos actualités ici -->
+                Plein de truc cool se sont passées. flash special au alentour de l\'allemange ou il paraitrait que d\'autre
+                truc ce passe. Vers l\'ouest rien d\'important mais un proffesseur aurait trouver quelque choses. Rien de sur.
+                c\'est tout pour aujourd\'hui nous essayeront de faire d\'autre truc demain.
             </div>
         </body>
     </html>
 ';
-
+$headers[] = 'MIME-Version: 1.0';
+$headers[] = 'Content-type: text/html; charset=iso-8859-1';
+$succeed = mail($to, "test avec html", $html, implode("\r\n", $headers));
